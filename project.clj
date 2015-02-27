0d76aaf1a45d43c1d05f0a395f564d50c7f46c24
Alex Redington <alex.redington@thinkrelevance.com>
2015-02-27 11:37:35 -0500

    Apraxified!

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject org.apraxis/pixels-against-humanity "0.0.1-SNAPSHOT"
+  :description "The Pixels Against Humanity Apraxis Demo Project"
+  :url "github.com/apraxis/apraxis"
+  :license {:name "MIT"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.apraxis/apraxis "0.0.1-SNAPSHOT"]])
