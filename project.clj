6acef675505a5cf0b69893405539a4b87dcc52c3
Alex Redington <alex.redington@thinkrelevance.com>
2015-04-03 12:11:23 -0400

    Start integrating apraxis.service

--- project.clj
+++ project.clj
@@ -3,4 +3,11 @@
   :url "github.com/apraxis/apraxis"
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.apraxis/apraxis "0.0.1-SNAPSHOT"]])
+  :dependencies [[org.apraxis/apraxis "0.0.1-SNAPSHOT"]
+                 [io.pedestal/pedestal.jetty "0.3.1"]
+                 [ch.qos.logback/logback-classic "1.1.2" :exclusions [org.slf4j/slf4j-api]]
+                 [org.slf4j/jul-to-slf4j "1.7.7"]
+                 [org.slf4j/jcl-over-slf4j "1.7.7"]
+                 [org.slf4j/log4j-over-slf4j "1.7.7"]]
+  :source-paths ["src/service"]
+  :plugins [[org.apraxis/apraxis "0.0.1-SNAPSHOT"]])
---
+++ src/service/pixels_against_humanity/service.clj
@@ -0,0 +1,41 @@
+(ns pixels-against-humanity.service
+  (:require [io.pedestal.http :as bootstrap]
+            [io.pedestal.http.route :as route]
+            [io.pedestal.http.body-params :as body-params]
+            [io.pedestal.http.route.definition :refer [expand-routes]]
+            [apraxis.service :as aservice]
+            [ring.util.response :as ring-resp]))
+
+(defn routes
+  []
+  (expand-routes
+   `[[["/dev" {:get aservice/dev-index}
+       ["/*dev-route" {:get aservice/dev-service}]]]]))
+
+;; Consumed by pixels-against-humanity2.server/create-server
+;; See bootstrap/default-interceptors for additional options you can configure
+(defn service
+  []
+  {:env :prod
+   ;; You can bring your own non-default interceptors. Make
+   ;; sure you include routing and set it up right for
+   ;; dev-mode. If you do, many other keys for configuring
+   ;; default interceptors will be ignored.
+   ;; :bootstrap/interceptors []
+   ::bootstrap/routes #(routes)
+
+   ;; Uncomment next line to enable CORS support, add
+   ;; string(s) specifying scheme, host and port for
+   ;; allowed source(s):
+   ;;
+   ;; "http://localhost:8080"
+   ;;
+   ;;::bootstrap/allowed-origins ["scheme://host:port"]
+
+   ;; Root for resource interceptor that is available by default.
+   ::bootstrap/resource-path "/public"
+
+   ;; Either :jetty, :immutant or :tomcat (see comments in project.clj)
+   ::bootstrap/type :jetty
+   ;;::bootstrap/host "localhost"
+   ::bootstrap/port 8080})
