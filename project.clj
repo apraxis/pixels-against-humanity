c4032636f0ecf4c7c5ced547ce9eff395e549c68
Joseph Smith <joe@uwcreations.com>
2015-08-28 14:57:22 -0500

    bump versions of things in project.clj

--- project.clj
+++ project.clj
@@ -3,8 +3,7 @@
   :url "github.com/apraxis/apraxis"
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2727"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.apraxis/apraxis "0.0.1-SNAPSHOT"]
                  [io.pedestal/pedestal.jetty "0.3.1"]
                  [ch.qos.logback/logback-classic "1.1.2" :exclusions [org.slf4j/slf4j-api]]
@@ -12,14 +11,13 @@
                  [org.slf4j/jcl-over-slf4j "1.7.7"]
                  [org.slf4j/log4j-over-slf4j "1.7.7"]
                  [om "0.8.0-alpha1"]
-                 [kioo "0.4.1-SNAPSHOT" :exclusions [com.keminglabs/cljx]]]
+                 [kioo "0.4.1-SNAPSHOT" :exclusions [org.clojure/tools.reader com.keminglabs/cljx]]]
   :source-paths ["src/service" "src/client/components"]
   :resource-paths ["target/middleman/build"
                    "target/apraxis-js"
                    "../apraxis/resources"
                    "resources"]
-  :plugins [[org.apraxis/apraxis "0.0.1-SNAPSHOT"]
-            [lein-cljsbuild "1.0.4"]]
+  :plugins [[org.apraxis/apraxis "0.0.1-SNAPSHOT"]]
   :cljsbuild {:builds [{:id "dev"
                         :source-paths ["src/client/components" "../apraxis/src/cljs"]
                         :compiler {:output-to "target/apraxis-js/js/pixels_against_humanity_client.js"
