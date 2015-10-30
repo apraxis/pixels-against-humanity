8525f1828e85025bc8c0c109c0bd3b3f5ff6a2bc
Michael Parenteau <prmparenteau@aol.com>
2015-10-30 09:14:52 -0700

    fixing interaction around card selection

--- src/client/components/pixels_against_humanity/prompt_and_answers.cljs
+++ src/client/components/pixels_against_humanity/prompt_and_answers.cljs
@@ -1,5 +1,5 @@
 (ns pixels-against-humanity.prompt-and-answers
-  (:require [kioo.om :refer [substitute content add-class]]
+  (:require [kioo.om :refer [do-> substitute content add-class]]
             [om.core :as om]
             [pixels-against-humanity.card :as card])
   (:require-macros [apraxis.client.template :refer [defsnippet]]))
@@ -12,8 +12,20 @@
    [:.prompt :.card] (substitute (om/build card/card-component (merge prompt {:type :prompt})))
    [:.cards] (let [answers (map #(merge % {:type :answer}) answers)
                    waiting (when (and waiting-count (pos? waiting-count))
-                             [{:type :waiting :count waiting-count}])]
-               (content (om/build-all card/card-component (concat answers waiting))))}
+                             [{:type :waiting :count waiting-count}])
+                   cards-class (cond
+                                 (and user-is-tzar
+                                      (pos? (count answers))
+                                      (empty? (filter :winner answers)))
+                                 "select-winner"
+                                 (and (not user-is-tzar)
+                                      (pos? (count answers))
+                                      (empty? (filter :selected answers))
+                                      (empty? (filter :winner answers))
+                                      (not (:winner prompt)))
+                                 "select-to-play")]
+               (do-> (add-class cards-class)
+                     (content (om/build-all card/card-component (concat answers waiting)))))}
   {:resource-wrapper :mini-html})
 
 (defn ^:export prompt-and-answers-component
